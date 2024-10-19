3cbaac5c28a5ab9176c0dfa58ecbdea7ff5f02de
klyushkin <stepan.9h88@mail.ru>
2024-10-19 14:18:11 +0300

    Create Coin and CoinUi models

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinLIstItem.kt
@@ -0,0 +1,14 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_list.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.plcoding.cryptotracker.crypto.presentation.models.CoinUi
+
+@Composable
+fun CoinListItem(
+    coinUi: CoinUi,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
